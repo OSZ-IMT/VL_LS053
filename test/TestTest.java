3fcda764e1aea230f4b6230bb6d7dc75833463e9
TrutzX <trutz@oszimt.de>
2024-03-18 17:06:00 +0100

    Initial commit

---
+++ test/TestTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Ignorieren Sie solange diese Datei, bis ihre Lehrkraft was dazu sagt.
+ */
+class TestTest {
+
+    /**
+     * Ignorieren Sie solange diese Datei, bis ihre Lehrkraft was dazu sagt.
+     * Tests sind ein optionales Zusatzfeature, welches nicht in jeder Klasse genutzt wird.
+     * Beispieltest, der nur testet ob Tests funktionieren.
+     */
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
